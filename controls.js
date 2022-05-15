09023f1ef520e552c7bc07ba77daf441166d9519
enverodie <enverodie@gmail.com>
2022-05-15 02:57:43 -0400

    Finished logic for draw origin relative to view

--- controls.js
+++ controls.js
@@ -1,16 +1,192 @@
-function handleKeypress(key) {
-    switch(key) {
+// active controls
+const ac = { 
+    spaceDown : false,
+    holdclick : false,
+}
+
+// gamestate
+const gs = {
+    dragging    : false,
+    playing     : false,
+    placeMode   : true,
+}
+
+// handle keyboard inputs
+
+function handleKeypress(press) {
+    switch(press.key) {
         case 'r': // reset
+            g.reset();
             break;
-        case 's': // start
-            break;
-        case 'p': // pause
+        case 's': // start/stop
+            playPause();
             break;
         default:
             break;
     }
 }
 
-function handleClick(event) {
+function handleKeydownUp(press) {
+    switch(press.key) {
+        case ' ':
+            spaceDownOnOff(press);
+            break;
+        default:
+            break;
+    }
+}
+
+// keyboard input helper functions
+
+function playPause() {
+    gs.playing = !gs.playing;
+    if (gs.playing) {
+        g.runLoop(); // needs to be restarted
+    }
+}
+
+// toggles the ability to drag the canvas
+function spaceDownOnOff(e) {
+    if (e.type == 'keydown') {
+        canvas.style.cursor = "grab";
+        ac.spaceDown = true;
+    } 
+    else if (e.type == 'keyup') {
+        canvas.style.cursor = "auto";
+        ac.spaceDown = false;
+    } 
+    else {
+        console.error(`event ${e} is not a valid spaceDownOnOff argument.`);
+        return;
+    } 
+}
+
+document.addEventListener('keypress', handleKeypress);
+document.addEventListener('keydown', handleKeydownUp);
+document.addEventListener('keyup', handleKeydownUp);
+
+let dragStart = { x: 0, y: 0 }
+
+// mouse event handlers
+
+// Gets the relevant location from a mouse or single touch event
+function getEventLocation(e) {
+    if (e.touches && e.touches.length == 1) {
+        return { x:e.touches[0].clientX, y: e.touches[0].clientY }
+    }
+    else if (e.clientX && e.clientY) {
+        return { x: e.clientX, y: e.clientY }        
+    }
+}
+
+// this function sets game states
+function onPointerDown(e) {
+    ac.holdclick = true;
+    // drag mode
+    if (ac.spaceDown) {
+        gs.dragging = true;
+        let sOrigX = canvas.width/2, sOrigY = canvas.height/2;
+        let pxDistX = ((cameraOffset.x*cameraZoom)-(sOrigX)*cameraZoom+sOrigX);
+        let pxDistY = ((cameraOffset.y*cameraZoom)-(sOrigY)*cameraZoom+sOrigY);
+        let bxDistX = Math.ceil(pxDistX / (scale * cameraZoom));
+        let bxDistY = Math.ceil(pxDistY / (scale * cameraZoom));
+        console.warn( pxDistX, pxDistY, bxDistX, bxDistY );
+        console.log(e.clientX, e.clientY);
+        // console.warn( ((e.clientX / scale) - (cameraOffset.x / scale)), ((e.clientY / scale) - (cameraOffset.y / scale)));
+        dragStart.x = getEventLocation(e).x/cameraZoom - cameraOffset.x;
+        dragStart.y = getEventLocation(e).y/cameraZoom - cameraOffset.y;
+    }
+    // place mode
+    if (!ac.spaceDown) { 
+        gs.placeMode = true;
+        placeSquare(e);
+    }
+}
+
+// this function resets gamestates and active controls
+function onPointerUp(e) { 
+    ac.holdclick = false;
+    if (gs.dragging) {
+        gs.dragging = false;
+        initialPinchDistance = null;
+        lastZoom = cameraZoom;
+    }
+}
+
+// this function checks gamestates
+function onPointerMove(e) {
+    if (gs.dragging) {
+        cameraOffset.x = getEventLocation(e).x/cameraZoom - dragStart.x; 
+        cameraOffset.y = getEventLocation(e).y/cameraZoom - dragStart.y;
+    }
+    if (gs.placeMode) {
+        placeSquare(e);
+    }
+}
+
+function handleTouch(e, singleTouchHandler) {
+    if ( e.touches.length == 1 ) {
+        singleTouchHandler(e)
+    }
+    else if (e.type == "touchmove" && e.touches.length == 2) {
+        gs.dragging = false
+        handlePinch(e)
+    }
+}
+
+let initialPinchDistance = null
+let lastZoom = cameraZoom
+
+function handlePinch(e) {
+    e.preventDefault()
     
+    let touch1 = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+    let touch2 = { x: e.touches[1].clientX, y: e.touches[1].clientY }
+    
+    // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
+    let currentDistance = (touch1.x - touch2.x)**2 + (touch1.y - touch2.y)**2
+    
+    if (initialPinchDistance == null) {
+        initialPinchDistance = currentDistance
+    }
+    else {
+        adjustZoom( null, currentDistance/initialPinchDistance )
+    }
+}
+
+function adjustZoom(zoomAmount, zoomFactor) {
+    if (!gs.dragging) {
+        if (zoomAmount) {
+            cameraZoom -= zoomAmount
+        }
+        else if (zoomFactor) {
+            console.log(zoomFactor)
+            cameraZoom = zoomFactor*lastZoom
+        }
+        
+        cameraZoom = Math.min( cameraZoom, MAX_ZOOM )
+        cameraZoom = Math.max( cameraZoom, MIN_ZOOM )
+        
+    }
+}
+
+canvas.addEventListener('mousedown', onPointerDown)
+canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
+canvas.addEventListener('mouseup', onPointerUp)
+canvas.addEventListener('touchend',  (e) => handleTouch(e, onPointerUp))
+canvas.addEventListener('mousemove', onPointerMove)
+canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove))
+canvas.addEventListener('wheel', (e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY))
+
+// mouse event listener helper functions
+
+function placeSquare(e) {
+    let originX = cameraOffset.x + (cameraZoom)*canvas.width;
+    let originY = cameraOffset.y + (cameraZoom)*canvas.height;
+    let x = e.clientX, y = e.clientY;
+
+    let distX = Math.floor((originX - x)/scale), distY = Math.floor((originY - y)/scale);
+    // console.log(distX, distY);
+    let originCoordX = originX / scale, originCoordY = originY / scale;
+
 }
--- grid.js
+++ grid.js
@@ -10,18 +10,24 @@ const g = {
     },
     runLoop : function() {
         const loopTime = 100;
-        setTimeout(() => {
-            this.calcNextGen();
-            g.runLoop();
+        if (gs.playing) setTimeout(() => {
+            if (gs.playing) {
+                this.calcNextGen();
+                g.runLoop();
+            } 
         }, loopTime);
     },
-    placeMode : true,
+    reset : function() {
+        g.living.clear();
+        g.nextGen.clear();
+    }
 }
 
 // lazy initialization
 function lazyInitGoL() {
-    for (let i = 0; i < 200; i++) {
-        const spawnRange = 100;
+    /*
+    for (let i = 0; i < 1000; i++) {
+        const spawnRange = 50;
         let coordinate1 = Math.floor(Math.random() * spawnRange);
         let coordinate2 = Math.floor(Math.random() * spawnRange);
         g.living.set(`${coordinate1},${coordinate2}`, new Square(coordinate1, coordinate2, 'blue'))
@@ -29,80 +35,88 @@ function lazyInitGoL() {
     lazyBlinker(10, 10);
     lazyBeacon(16, 10);
     lazyPentaDecathlon(32, 10);
-    g.runLoop();
+    */
+   g.living.set('0,0', new Square(0,0, 'blue'));
+   g.living.set('95,0', new Square(95,0, 'blue'));
+   g.living.set('0,47', new Square(0,47, 'blue'));
+   g.living.set('95,47', new Square(95,47, 'blue'));
+   g.runLoop();
 }
 lazyInitGoL();
 
 // Test structures for GoL
 
 function lazyBlinker(xcenter, ycenter) {
-    g.living.set(`${xcenter - 1},${ycenter}`, new Square(xcenter - 1, ycenter, 'blue'));
-    g.living.set(`${xcenter    },${ycenter}`, new Square(xcenter    , ycenter, 'blue'));
-    g.living.set(`${xcenter + 1},${ycenter}`, new Square(xcenter + 1, ycenter, 'blue'));
+    const color = 'rgb(155, 0, 0)';
+    g.living.set(`${xcenter - 1},${ycenter}`, new Square(xcenter - 1, ycenter, color));
+    g.living.set(`${xcenter    },${ycenter}`, new Square(xcenter    , ycenter, color));
+    g.living.set(`${xcenter + 1},${ycenter}`, new Square(xcenter + 1, ycenter, color));
 }
 
 function lazyBeacon(xtop, ytop) {
-    g.living.set(`${xtop},${ytop}`, new Square(xtop, ytop, 'blue'));
-    g.living.set(`${xtop},${ytop-1}`, new Square(xtop, ytop-1, 'blue'));
-    g.living.set(`${xtop+1},${ytop}`, new Square(xtop+1, ytop, 'blue'));
+    const color = 'red';
+    g.living.set(`${xtop},${ytop}`, new Square(xtop, ytop, color));
+    g.living.set(`${xtop},${ytop-1}`, new Square(xtop, ytop-1, color));
+    g.living.set(`${xtop+1},${ytop}`, new Square(xtop+1, ytop, color));
     
-    g.living.set(`${xtop+3},${ytop-3}`, new Square(xtop+3, ytop-3, 'blue'));
-    g.living.set(`${xtop+2},${ytop-3}`, new Square(xtop+2, ytop-3, 'blue'));
-    g.living.set(`${xtop+3},${ytop-2}`, new Square(xtop+3, ytop-2, 'blue'));   
+    g.living.set(`${xtop+3},${ytop-3}`, new Square(xtop+3, ytop-3, color));
+    g.living.set(`${xtop+2},${ytop-3}`, new Square(xtop+2, ytop-3, color));
+    g.living.set(`${xtop+3},${ytop-2}`, new Square(xtop+3, ytop-2, color));   
 }
 
 function lazyPentaDecathlon(xcenter, ytop) {
+    const color = 'green';
     var x = xcenter - 1, y = ytop;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter + 1, y = ytop;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     
     var x = xcenter, y = ytop - 1;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop - 2;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     
     var x = xcenter - 1, y = ytop - 3;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop - 3;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter + 1, y = ytop - 3;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     
     var x = xcenter - 1, y = ytop - 5;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop - 5;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter + 1, y = ytop - 5;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter - 1, y = ytop - 6;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop - 6;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter + 1, y = ytop - 6;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));  
+    g.living.set(`${x},${y}`, new Square(x, y, color));  
     
     var bottom = 8;
     var x = xcenter - 1, y = ytop - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter + 1, y = ytop - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     
     var x = xcenter, y = ytop - 1 - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop - 2 - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     
     var x = xcenter - 1, y = ytop - 3 - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter, y = ytop - 3 - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));   
+    g.living.set(`${x},${y}`, new Square(x, y, color));   
     var x = xcenter + 1, y = ytop - 3 - bottom;
-    g.living.set(`${x},${y}`, new Square(x, y, 'blue'));  
+    g.living.set(`${x},${y}`, new Square(x, y, color));  
 }
 
 // not currently in use
